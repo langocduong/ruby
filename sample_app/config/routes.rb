4eaf51355158f856b25954660696e44eccb4fae1
langocduong <langocduong1998@gmail.com>
2019-12-29 22:17:04 +0700

    sampleapp

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  get 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :relationships,       only: [:create, :destroy]
+  resources :microposts, only:[:create, :destroy]
 end
